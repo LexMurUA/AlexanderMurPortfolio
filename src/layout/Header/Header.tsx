c4a8207e73f0eac8228f51c234172ab928ca6e33
LexMurUa <lex16mur@gmail.com>
2025-10-11 15:02:17 +0300

    build

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import me from '../../assets/me.png';
 import './Header.css';
-
+import burger from '../../assets/burger.svg'
+import burgeropen from '../../assets/openburg.gif'
 
 export const Header = () => {
     const [openMenu, setOpenMenu] = useState(false)
@@ -9,22 +10,33 @@ export const Header = () => {
         setOpenMenu(prev => !prev)
     }
     return (
-        <header className='container px-9 py-0 fixed top-0 left-0 bg-red-600 border border-red-800 '>
-            <div className='flex justify-between sm:justify-around ' >
-                <div className='flex items-center border border-amber-100 p-2 gap-2'>
-                    <img src={me} alt="Myfoto" className='border rounded-full w-20 h-20 object-cover ' />
-                    <span className='w-full text-adapt border font-medium text-red-900'>Alexander Murakin</span>
+        <>
+            <div className='h-[20vh] '></div>
+            <header className='container fixed justify-center lg:justify-between   bg-neutral-50 flex flex-col lg:flex-row justify-between px-0 h-[20vh] py-0  top-0 left-0'>
+
+                <div className='flex    ' >
+                    <div className='w-1/2 flex items-center p-2 gap-2 '>
+                        <img src={me} alt="Myfoto" className=' rounded-full w-20 h-20 object-cover ' />
+                        <span className='w-full text-adapt  font-medium text-red-900'>Alexander Murakin</span>
+                    </div>
+                    <div className='w-1/2 p-2 flex items-center justify-end lg:hidden'>
+                        <img src={openMenu ? burgeropen : burger} onClick={toggle} className='lg:hidden ' alt="Menu" />
+                    </div>
+
+
                 </div>
-
-                <nav className={`flex items-center border-green-700 gap-16 text-lg font-medium text-blue-400 text-adapt sm:absolute left-[${openMenu ? '50%' : '100%'}]`} >
-                    <a href="" className='link-nav '>Home</a>
-                    <a href="" className='link-nav'>About</a>
-                    <a href="" className='link-nav'>Projects</a>
-                    <a href="" className='link-nav'>Contacts</a>
+                <nav className={` bg-neutral-50 flex w-full justify-evenly  
+                absolute lg:static top-[100%] ${openMenu ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-full'}
+                 transition-all duration-300  font-medium text-blue-400 text-adapt 
+                  lg:w-[45%] lg:items-center `} >
+                    <a href="" className='text-adapt'>Home</a>
+                    <a href="" className='text-adapt'>About</a>
+                    <a href="" className='text-adapt'>Projects</a>
+                    <a href="" className='text-adapt'>Contacts</a>
                 </nav>
-                <button onClick={toggle}>P</button>
-            </div>
 
-        </header>
+
+            </header>
+        </>
     )
 }
--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -3,6 +3,6 @@ import './Main.css';
 export const Main = ()=>{
 
     return(
-        <main className='container bg-amber-500 xl: w-[100%] h-[200vh]'></main>
+        <main className='container bg-amber-500 xl: w-[100%] h-[200vh]'>jhbjh</main>
     )
 }
